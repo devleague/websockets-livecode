671728ff659c675aa53f7e8dfb69d7f05d136355
Edward Kim <taesup63@gmail.com>
2019-06-05 14:42:04 -1000

    Initial Draft

---
+++ README.md
@@ -0,0 +1,14 @@
+## Websocket Livecode
+
+Mainly used [Socket.io](https://socket.io) as a Websocket platform but Socket.io does much more than just websockets.
+
+### Authentication
+
+I do not deal with authenticating users through websockets, you can find more information about that here: https://stackoverflow.com/questions/19548009/user-authentication-using-socket-io
+
+[socketio-jwt](https://github.com/auth0-community/socketio-jwt)
+[jsonwebtoken](https://github.com/auth0/node-jsonwebtoken)
+
+### Demo
+
+`nodemon server.js`
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+const PORT = 8080;
+const onlineUsers = {};
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/message', (req, res) => {});
+
+// websocket connection
+io.on('connection', (socket) => {
+  console.log('a connection was made');
+
+  // user identification
+  socket.on('identification', (msg) => {
+    console.log('ident', msg);
+
+    // add socket to onlineUsers object
+    onlineUsers[msg.id] = socket;
+    socket.user = msg;
+
+    // send back identification verification
+    socket.emit('verified', true);
+  });
+
+  // disconnect event
+  socket.on('disconnect', () => {
+    const { user } = socket;
+
+    if (user && user.id) {
+      delete onlineUsers[user.id];
+    }
+
+    console.log('a user disconnected');
+  });
+
+  // user chat message
+  socket.on('chat', (msg) => {
+    console.log('chat message:', msg);
+    // this is where you would save to a DB
+
+    // then emit out to other chat members
+    const intendedUser = onlineUsers[msg.to];
+
+    if (intendedUser) {
+      intendedUser.emit('chat', msg);
+    }
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
